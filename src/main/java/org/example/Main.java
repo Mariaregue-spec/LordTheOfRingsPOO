e036966da2f71481e40f2746238919077062a914
Mariaregue-spec <mariareguecurso@gmail.com>
2026-02-24 15:24:20 +0100

    Add Character array to store 15 instances

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,7 @@
 package org.example;
 
-A
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!"  );
+        Character[] characters = new Character[15];
     }
 }
