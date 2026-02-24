d8fb239002043adf9fbb337ebbdea1a4b43422e0
Mariaregue-spec <mariareguecurso@gmail.com>
2026-02-24 13:00:21 +0100

    Add Character.java

---
+++ src/main/java/org/example/Character.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Character {
+    private String name;
+    private int age;
+    private String origin;
+    private String alignment;
+    private String race;
+    private String description;
+    private String image;
+    public Character(String name, int age, String origin, String alignment, String race, String description, String image) {
+        this.name = name;
+        this.age = age;
+        this.origin = origin;
+        this.alignment = alignment;
+        this.race = race;
+        this.description = description;
+        this.image = image;
+    }
+}
