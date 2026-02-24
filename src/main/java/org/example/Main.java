a8b57c58a06edbb73f16a90fe449438b69ab4672
Mariaregue-spec <mariareguecurso@gmail.com>
2026-02-24 15:54:19 +0100

    Add three Elf characters to Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,5 +14,17 @@ public class Main {
         characters[2] = new Human("Eomer", 36, "Rohan", "Bueno",
                 "Mariscal de la Marca de los Jinetes", "eomer.png",
                 "Rohan", "Mariscal", true);
+        // 3 Elves
+        characters[3] = new Elf("Legolas", 2931, "Bosque de Greenwood", "Bueno",
+                "Príncipe del Bosque y gran arquero", "legolas.png",
+                "Bosque", true, "Bosque de Greenwood");
+
+        characters[4] = new Elf("Elrond", 6500, "Rivendel", "Bueno",
+                "Señor de Rivendel", "elrond.png",
+                "Elfos de Rivendel", true, "Rivendel");
+
+        characters[5] = new Elf("Galadriel", 7000, "Lothlórien", "Bueno",
+                "Dama de Lothlórien", "galadriel.png",
+                "Elfos de Lothlórien", true, "Lothlórien");
     }
 }
