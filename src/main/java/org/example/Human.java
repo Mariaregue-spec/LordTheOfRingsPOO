cf25ae82c6b0ce4f8ca35a9fa487d62f7cef43e9
Mariaregue-spec <mariareguecurso@gmail.com>
2026-02-24 14:09:26 +0100

    Create Human subclass with polymorphism implementation

---
+++ src/main/java/org/example/Human.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class Human extends Character {
+   private String kingdom;
+   private String title;
+   private boolean isNoble;
+   public Human(String name, int age, String origin, String alignment, String description, String image, String kingdom, String title, boolean isNoble) {
+       super(name,age,origin,alignment,"Human",description,image);
+
+       this.kingdom = kingdom;
+       this.title = title;
+       this.isNoble = isNoble;
+    }
+    public String getKingdom() {
+        return kingdom;
+    }
+    public String getTitle() {
+       return title;
+    }
+    public boolean isNoble() {
+       return isNoble;
+    }
+    public void setKingdom(String kingdom) {
+        this.kingdom = kingdom;
+    }
+    public void setTitle(String title) {
+       this.title = title;
+    }
+    public void setIsNoble(boolean isNoble) {
+       this.isNoble = isNoble;
+    }
+    @Override
+    public void displayDetails() {
+        System.out.println("Race: " + getRace() +
+                " | Name: " + getName() +
+                " | Kingdom: " + kingdom +
+                " | Title: " + title +
+                " | Noble: " + isNoble +
+                " | Description: " + getDescription() +
+                " | Image: " + getImage());
+    }
+}
