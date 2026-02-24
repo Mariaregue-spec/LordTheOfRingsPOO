a5b305ef97703b2851b7e5fa88c01212bc9abaf7
Mariaregue-spec <mariareguecurso@gmail.com>
2026-02-24 14:18:24 +0100

    Add Dwarf subclass with specific attributes and polymorphism implementation

---
+++ src/main/java/org/example/Dwarf.java
@@ -0,0 +1,54 @@
+package org.example;
+
+public class Dwarf extends Character {
+
+    private String house;
+    private String weapon;
+    private double beardLength;
+
+    public Dwarf(String name, int age, String origin, String alignment,
+                 String description, String image,
+                 String house, String weapon, double beardLength) {
+
+        super(name, age, origin, alignment, "Dwarf", description, image);
+
+        this.house = house;
+        this.weapon = weapon;
+        this.beardLength = beardLength;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public double getBeardLength() {
+        return beardLength;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public void setBeardLength(double beardLength) {
+        this.beardLength = beardLength;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Race: " + getRace() +
+                " | Name: " + getName() +
+                " | House: " + house +
+                " | Weapon: " + weapon +
+                " | Beard Length: " + beardLength +
+                " | Description: " + getDescription() +
+                " | Image: " + getImage());
+    }
+}
