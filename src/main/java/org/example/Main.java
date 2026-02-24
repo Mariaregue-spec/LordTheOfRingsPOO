bad715c0891685998ddd232cbad64e9cae8e261f
Mariaregue-spec <mariareguecurso@gmail.com>
2026-02-24 15:11:22 +0100

    Add Wizard subclass with specific attributes and polymorphism implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-
+A
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!"  );
---
+++ src/main/java/org/example/Wizard.java
@@ -0,0 +1,54 @@
+package org.example;
+
+public class Wizard extends Character {
+
+    private String staffType;
+    private String robeColor;
+    private int powerLevel;
+
+    public Wizard(String name, int age, String origin, String alignment,
+                  String description, String image,
+                  String staffType, String robeColor, int powerLevel) {
+
+        super(name, age, origin, alignment, "Wizard", description, image);
+
+        this.staffType = staffType;
+        this.robeColor = robeColor;
+        this.powerLevel = powerLevel;
+    }
+
+    public String getStaffType() {
+        return staffType;
+    }
+
+    public String getRobeColor() {
+        return robeColor;
+    }
+
+    public int getPowerLevel() {
+        return powerLevel;
+    }
+
+    public void setStaffType(String staffType) {
+        this.staffType = staffType;
+    }
+
+    public void setRobeColor(String robeColor) {
+        this.robeColor = robeColor;
+    }
+
+    public void setPowerLevel(int powerLevel) {
+        this.powerLevel = powerLevel;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Race: " + getRace() +
+                " | Name: " + getName() +
+                " | Staff Type: " + staffType +
+                " | Robe Color: " + robeColor +
+                " | Power Level: " + powerLevel +
+                " | Description: " + getDescription() +
+                " | Image: " + getImage());
+    }
+}
