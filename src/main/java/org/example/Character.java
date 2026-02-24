8c383bdc60030b8255a23d298a8808dfeb1bc2be
Mariaregue-spec <mariareguecurso@gmail.com>
2026-02-24 13:50:21 +0100

    Complete Character class with getters, setters and abstract method

--- src/main/java/org/example/Character.java
+++ src/main/java/org/example/Character.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Character {
+public abstract class Character {
     private String name;
     private int age;
     private String origin;
@@ -8,6 +8,7 @@ public class Character {
     private String race;
     private String description;
     private String image;
+
     public Character(String name, int age, String origin, String alignment, String race, String description, String image) {
         this.name = name;
         this.age = age;
@@ -17,4 +18,62 @@ public class Character {
         this.description = description;
         this.image = image;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getAlignment() {
+        return alignment;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public void setAlignment(String alignment) {
+        this.alignment = alignment;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public abstract void displayDetails();
 }
